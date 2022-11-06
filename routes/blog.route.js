c8d6e39a7b6688dccab39390d4a44871032bc8fd
Asuquo Emmanuel <emmanueldan0920@gmail.com>
2022-11-06 21:15:30 +0100

    Update blog.route.js

--- routes/blog.route.js
+++ routes/blog.route.js
@@ -1,9 +1,14 @@
 const express = require('express')
 const router = express.Router()
 const { createBlog, getBlog, getAllBlogs, updateBlog, deleteBlog } = require('../controllers/blog.controller')
+const { filterAndSort, filterByPublished, list, setUserFilter } = require('../middleware/filtering')
 const verifyToken = require('../middlewares/verifyToken')
+const pagination = require('../middleware/pagination')
+const isCreator = require('../middleware/isCreator')
 
-router.get('/', getAllBlogs)
+router.route('/')
+    .get('/', getAllBlogs)
+    .get( filterAndSort, setUserFilter, pagination, blogController.getBlogs)
 
 router.post('/create', verifyToken, createBlog)
 
